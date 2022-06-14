2c9f4ff3db3610cef0b17cd9486709b630482886
Guillem B <guillembaracco@gmail.com>
2022-06-14 17:23:52 +0200

    iteration 3 in process

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
 
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+// all your routes here
+
+/*
+router.get("/celebrities", requiere ("./celebrities.routes"));
+
+router.get("/movies",requiere ("./movies.routes") );
+*/
 
 module.exports = router;
