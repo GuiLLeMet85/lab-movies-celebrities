9cfcd434913cdea43e47f8706cf6d1bc634e8fc2
Guillem B <guillembaracco@hotmail.com>
2022-06-27 18:05:31 +0200

    iteration 4 in process

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -4,23 +4,34 @@ const Celebrity = require('../models/Celebrity.model');
 
 // all your routes here
 
+// GET celebrities page 
+router.get("/", async(req, res, next) => {
+    const celebrities = await Celebrity.find({});
+    try {
+        res.render("celebrities/celebrities", { celebrities });
+    } catch (error) {
+        next(error);
+    }
+})
+
+// Add a new celebrities
 router.get('/create', (req, res, next) => {
     try {
         res.render('celebrities/new-celebrity');
-    } 
-    catch (error) {
+    } catch (error) {
         next(error)
     }
 });
 
-router.post('/create', async (req, res, next) => {
+router.post('/create', async(req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+    console.log(req.body);
     try {
-        const {name, occupation, catchPhrase} = req.body;
-        await Celebrity.create({name, occupation, catchPhrase});
+        await Celebrity.create({ name, occupation, catchPhrase });
+        res.redirect("/celebrities");
+    } catch (error) {
+        res.redirect("/celebrities/create");
     }
-    catch (error) {
-        next(error)
-    }
-});
+})
 
 module.exports = router;
